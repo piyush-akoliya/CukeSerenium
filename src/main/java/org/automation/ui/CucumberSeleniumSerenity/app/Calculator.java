f36d0ac56a3faa647856ecafc0c2c1793418da28
Piyush Akoliya <piyushakoliya@piyushs-air.home>
2024-05-10 02:19:32 -0300

    Cucumber serenity Setup

---
+++ src/main/java/org/automation/ui/CucumberSeleniumSerenity/app/Calculator.java
@@ -0,0 +1,7 @@
+package org.automation.ui.CucumberSeleniumSerenity.app;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/org/automation/ui/CucumberSeleniumSerenity/acceptancetests/AcceptanceTestSuite.java
@@ -0,0 +1,9 @@
+package org.automation.ui.CucumberSeleniumSerenity.acceptancetests;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/", glue="org.automation.ui.CucumberSeleniumSerenity")
+public class AcceptanceTestSuite {}
