236a583f1f0afdfef0ead002ca358550f2830160
Piyush Akoliya <piyushakoliya@piyushs-air.home>
2024-05-11 11:46:14 -0300

    Created a common Serenity Class for taing screenshots

--- src/test/java/org/automation/ui/CucumberSeleniumSerenity/steps/serenity/EndUserSteps.java
+++ src/test/java/org/automation/ui/CucumberSeleniumSerenity/steps/serenity/EndUserSteps.java
@@ -1,17 +1,37 @@
 package org.automation.ui.CucumberSeleniumSerenity.steps.serenity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.automation.ui.CucumberSeleniumSerenity.pages.HomePage;
+import org.junit.runner.RunWith;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Step;
 
+@RunWith(SerenityRunner.class)
 public class EndUserSteps {
-	
+
+	HomePage homePage = new HomePage("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+
+	public EndUserSteps() {
+	}
+
 	@Step
 	public void loadingShoeWebsite() {
-		
+		System.out.println("The page loaded ya'll");
+		System.out.println(homePage.getMainPageText());
+		System.out.println(homePage.getShoePortalTitle());
+		if (homePage.getShoePortalTitle() != null)
+			assertTrue(true);
+		else
+			assertTrue(false);
 	}
-	
+
 	@Step
-	public void checkHomePageTitle(String homePageTitle) {
-		// TODO Auto-generated method stub
-		
+	public void checkHomePageTitle() {
+	    assertEquals("Ace Online Shoe Portal", homePage.getShoePortalTitle());
 	}
+
+
 }
