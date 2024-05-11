7c522b938dfd2603b947e2120a18f9a3ca2ed299
Piyush Akoliya <piyushakoliya@piyushs-air.home>
2024-05-11 11:43:16 -0300

    Created step definition file for homepage

--- src/test/java/org/automation/ui/CucumberSeleniumSerenity/steps/HamburgerMenuAvailability.java
+++ src/test/java/org/automation/ui/CucumberSeleniumSerenity/steps/HamburgerMenuAvailability.java
@@ -1,30 +1,24 @@
 package org.automation.ui.CucumberSeleniumSerenity.steps;
 
+import org.automation.ui.CucumberSeleniumSerenity.steps.serenity.EndUserSteps;
+
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import org.automation.ui.CucumberSeleniumSerenity.steps.serenity.EndUserSteps;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class HamburgerMenuAvailability {
 
-	@Autowired
-	EndUserSteps endUserSteps;
+	EndUserSteps endUserSteps=new EndUserSteps();
 	
-    @Given("User navigates to the Ace Online Shoe Portal page")
+
+	@Given("User navigates to the Ace Online Shoe Portal page")
     public void loadingShoeWebsite() {
     	endUserSteps.loadingShoeWebsite();
     }
 
-    @When("the title of the page should be {String}")
-    public void checkHomePageTitle(String homePageTitle) {
-    	endUserSteps.checkHomePageTitle(homePageTitle);
-    }
-
-    @Then("the test {String} is displayed")
-    public void displayHamburgerOptions(String displayText) {
-    	
-    }
+	@When("the title of the page should be correct")
+	public void the_title_of_the_page_should_be_correct() {
+		endUserSteps.checkHomePageTitle();
+	}
 
 }
